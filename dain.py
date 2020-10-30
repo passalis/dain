5a28cbc50878fab710db9607f5b74d9466412d5d
Marek Otahal <marek.otahal@softvision.ai>
2020-10-30 21:03:53 +0100

    do not duplicate codepath for DAIN 1+2+3
    do not repeat the same code (for easier changes in one place only)

--- dain.py
+++ dain.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 class DAIN_Layer(nn.Module):
     def __init__(self, mode='adaptive_avg', mean_lr=0.00001, gate_lr=0.001, scale_lr=0.00001, input_dim=144):
         super(DAIN_Layer, self).__init__()
+        assert mode in [None, 'avg', 'adaptive_avg', 'adaptive_scale', 'full'], f'Unsupported mode: {mode}!'\
+                'Use one of: None, "avg", "adaptive_avg", "adaptive_scale", "full". '
         print("Mode = ", mode)
 
         self.mode = mode
@@ -32,64 +34,46 @@ class DAIN_Layer(nn.Module):
     def forward(self, x):
         # Expecting  (n_samples, dim,  n_feature_vectors)
 
+        ## Other methods:
         # Nothing to normalize
         if self.mode == None:
-            pass
-
+            return x
         # Do simple average normalization
         elif self.mode == 'avg':
             avg = torch.mean(x, 2)
             avg = avg.resize(avg.size(0), avg.size(1), 1)
             x = x - avg
+            return x
 
-        # Perform only the first step (adaptive averaging)
-        elif self.mode == 'adaptive_avg':
-            avg = torch.mean(x, 2)
-            adaptive_avg = self.mean_layer(avg)
-            adaptive_avg = adaptive_avg.resize(adaptive_avg.size(0), adaptive_avg.size(1), 1)
-            x = x - adaptive_avg
+        ## DAIN:
+        # Perform the first step:   adaptive averaging; DAIN(1)
+        # Step 1:
+        avg = torch.mean(x, 2)
+        adaptive_avg = self.mean_layer(avg)
+        adaptive_avg = adaptive_avg.resize(adaptive_avg.size(0), adaptive_avg.size(1), 1)
+        x = x - adaptive_avg
+        if self.mode == 'adaptive_avg':
+            return x
 
-        # Perform the first + second step (adaptive averaging + adaptive scaling )
-        elif self.mode == 'adaptive_scale':
-            # Step 1:
-            avg = torch.mean(x, 2)
-            adaptive_avg = self.mean_layer(avg)
-            adaptive_avg = adaptive_avg.resize(adaptive_avg.size(0), adaptive_avg.size(1), 1)
-            x = x - adaptive_avg
+        # Perform the second step:  adaptive averaging + adaptive scaling; DAIN(1+2)
+        # Step 2:
+        std = torch.mean(x ** 2, 2)
+        std = torch.sqrt(std + self.eps)
+        adaptive_std = self.scaling_layer(std)
+        adaptive_std[adaptive_std <= self.eps] = 1
 
-            # Step 2:
-            std = torch.mean(x ** 2, 2)
-            std = torch.sqrt(std + self.eps)
-            adaptive_std = self.scaling_layer(std)
-            adaptive_std[adaptive_std <= self.eps] = 1
+        adaptive_std = adaptive_std.resize(adaptive_std.size(0), adaptive_std.size(1), 1)
+        x = x / adaptive_std
+        if self.mode == 'adaptive_scale':
+            return x
 
-            adaptive_std = adaptive_std.resize(adaptive_std.size(0), adaptive_std.size(1), 1)
-            x = x / (adaptive_std)
+        # Perform the third step:   adaptuve avg + adative scale + gating; DAIN(1+2+3)
+        # Step 3: 
+        avg = torch.mean(x, 2)
+        gate = F.sigmoid(self.gating_layer(avg))
+        gate = gate.resize(gate.size(0), gate.size(1), 1)
+        x = x * gate
+        if self.mode == 'full':
+            return x
 
-        elif self.mode == 'full':
-
-            # Step 1:
-            avg = torch.mean(x, 2)
-            adaptive_avg = self.mean_layer(avg)
-            adaptive_avg = adaptive_avg.resize(adaptive_avg.size(0), adaptive_avg.size(1), 1)
-            x = x - adaptive_avg
-
-            # # Step 2:
-            std = torch.mean(x ** 2, 2)
-            std = torch.sqrt(std + self.eps)
-            adaptive_std = self.scaling_layer(std)
-            adaptive_std[adaptive_std <= self.eps] = 1
-
-            adaptive_std = adaptive_std.resize(adaptive_std.size(0), adaptive_std.size(1), 1)
-            x = x / adaptive_std
-
-            # Step 3: 
-            avg = torch.mean(x, 2)
-            gate = F.sigmoid(self.gating_layer(avg))
-            gate = gate.resize(gate.size(0), gate.size(1), 1)
-            x = x * gate
-
-        else:
-            assert False
-
-        return x
+        assert False, "You fool! Should not reach here."
